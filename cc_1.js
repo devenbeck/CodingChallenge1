80b112d4095e12b0f85cc1b6b9ff19db02e7053c
Natalie Devenbeck <devenbeck@usf.edu>
2025-01-26 16:41:57 -0500

    Task 1-Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+//Task 1: Variables //
+let employeeName = "Alan Jackson";
+const employeeID = 1;
+var isActive = true;
+
+console.log(employeeName); //Alan Jackson
+console.log(typeof employeeName); //string
+console.log(employeeID); //1
+console.log(typeof employeeID);//number
+console.log(isActive);//true
+console.log(typeof isActive);//boolean
