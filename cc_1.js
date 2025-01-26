d6b0c59a4d5b645e1b4cba187de3843e4dc64224
Natalie Devenbeck <devenbeck@usf.edu>
2025-01-26 16:55:00 -0500

    Task 2- Product Details

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,15 @@ console.log(employeeID); //1
 console.log(typeof employeeID);//number
 console.log(isActive);//true
 console.log(typeof isActive);//boolean
+
+//Task 2: Primitive Data Types
+let productName = "hair dryer 4000";
+const productPrice = 85;
+var isAvailable = true;
+
+console.log(productName); //Hair Dryer 4000
+console.log(typeof productName); //string
+console.log(productPrice); //85
+console.log(typeof productPrice); //number
+console.log(isAvailable); //true
+console.log(typeof isAvailable); //boolean
